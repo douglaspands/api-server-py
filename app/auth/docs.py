20152e9321adc6a81293c226ea5d6fc0b171204c
Douglas Panhota <douglaspands@gmail.com>
2021-07-28 02:47:59 -0300

    inclusao de docs para a api auth

---
+++ app/auth/docs.py
@@ -0,0 +1,14 @@
+"""Users OpenApiDocs."""
+from typing import Any, Dict
+
+get_token: Dict[str, Any] = {
+    'name': 'Get token.',
+    'description': 'Get token for system access.',
+    'responses': {
+        400: {'description': 'Bad request.'},
+        401: {'description': 'Unauthorized access.'}
+    }
+}
+
+
+__all__ = ('get_token',)
